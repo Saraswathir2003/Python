c4f3c8927817af4feadadd737e0ec105202356ab
Saraswathir2003 <saraswathir003@gmail.com>
2026-07-20 22:32:42 +0530

    Add files via upload

---
+++ Day1/Day1.py
@@ -0,0 +1,34 @@
+Place=input("Enter The Place")
+Student_name=input("Enter Student Name")
+Student_age=input("Enter Student Age")
+Department=input("Enter Department")
+College=input("Enter College Name")
+Year=int(input("Enter The Year"))
+Average=float(input("Enter The Average"))
+Gender=input("Enter The Gender")
+Phone=int(input("Enter the Phone Number"))
+Email=input("Enter The Email")
+
+
+print("Enter The Place" , Place)
+print("Enter Student Name", Student_name)
+print("Enter Student Age",Student_age)
+print("Enter Department",Department)
+print("Enter College Name",College)
+print("Enter Year", Year)
+print("Enter The Average",Average)
+print("Enter gender",Gender)
+print("Enter Phone Number",Phone)
+print("Enter The Email",Email)
+
+
+Mark1=int(input("Enter the Mark1"))
+Mark2=int(input("Enter the Mark2"))
+Mark3=int(input("Enter the Mark3"))
+
+TotalMark = Mark1 + Mark2 + Mark3 /3
+
+print("Enter The Mark1",Mark1)
+print("Enter the Mark2",Mark2)
+print("Enter the Mark3", Mark3)
+print("TotalMarks",TotalMark)
