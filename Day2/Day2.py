6da3337df1495f9bf1b10abc76eeea51e460221b
Saraswathir2003 <saraswathir003@gmail.com>
2026-07-22 11:29:12 +0530

    Add files via upload

---
+++ Day2/Day2.py
@@ -0,0 +1,11 @@
+#1.Write a program for any event happened today in ur life using operators
+
+BusNumbers=[17,25,26,24,37,12,72]
+Bus=int(input("Enter the Bus Number"))
+
+if Bus in BusNumbers :
+    print("Bus is Coming")
+else:
+    print("Bus is Not Coming")
+
+
