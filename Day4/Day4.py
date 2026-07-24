fe57f2b1e9ccadb12682d5a497ebbdd384619e6f
Saraswathir2003 <saraswathir003@gmail.com>
2026-07-24 11:38:29 +0530

    Add files via upload

---
+++ Day4/Day4.py
@@ -0,0 +1,11 @@
+count=1
+while count<=9:
+    if count == 3:
+        print(30,end=" ")
+    elif count == 6:
+        print(60, end=" ")
+    elif count == 9:
+        print(90, end=" ")
+    else:
+        print(count,end=" ")
+    count +=1
